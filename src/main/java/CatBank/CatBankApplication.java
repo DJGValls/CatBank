72a899cdb9fe9f41d8eda95387f08a613e8a90e3
Daniel <jiga1980@gmail.com>
2022-09-23 11:10:46 +0200

    Update 1 - Creation github project

---
+++ src/main/java/CatBank/CatBankApplication.java
@@ -0,0 +1,13 @@
+package CatBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CatBank/CatBankApplicationTests.java
@@ -0,0 +1,13 @@
+package CatBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
