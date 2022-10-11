5141336f317165cb9a82a1bbab9797439d7ca49b
Daniel <jiga1980@gmail.com>
2022-10-11 21:05:05 +0200

    Update 21 - CreditCard account implemented

--- src/main/java/CatBank/Controller/CreditCardController.java
+++ src/main/java/CatBank/Controller/CreditCardController.java
@@ -1,14 +1,17 @@
 package CatBank.Controller;
 
+
 import CatBank.Model.DTO.FactoryAccountDTO;
 import CatBank.Model.DTO.TransferenceDTO;
 import CatBank.Model.User.AccountHolder;
 import CatBank.Repository.CheckingRepository;
+import CatBank.Repository.CreditCardRepository;
 import CatBank.Security.DTO.MensajeDTO;
 import CatBank.Security.JasonWebToken.JwtProvider;
 import CatBank.Security.Service.RoleService;
 import CatBank.Security.Service.UserService;
 import CatBank.Service.AccountHolderService;
+import CatBank.Service.CreditCardService;
 import CatBank.Service.SavingsService;
 import CatBank.Service.StudentCheckingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,67 +26,61 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/savings")
+@RequestMapping("/creditCard")
 @CrossOrigin
-public class SavingsController {
+public class CreditCardController {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-
     @Autowired
     AuthenticationManager authenticationManager;
-
     @Autowired
     UserService userService;
-
     @Autowired
     RoleService roleService;
-
     @Autowired
     StudentCheckingService studentCheckingService;
-
     @Autowired
     JwtProvider jwtProvider;
-
     @Autowired
     CheckingRepository checkingRepository;
-
     @Autowired
     AccountHolderService accountHolderService;
-
+    @Autowired
+    CreditCardService creditCardService;
     @Autowired
     SavingsService savingsService;
 
     @PreAuthorize("hasRole('ACCOUNTHOLDER')")
-    @GetMapping("/balance/{savingsId}")
-    public ResponseEntity getBalance(@PathVariable(value = "savingsId") int savingsId){
-        return savingsService.getBalance(savingsId);
+    @GetMapping("/balance/{creditCardId}")
+    public ResponseEntity getBalance(@PathVariable(value = "creditCardId") int creditCardId){
+        return creditCardService.getBalance(creditCardId);
     }
 
     @PreAuthorize("hasRoles('ACCOUNTHOLDER','THIRDPARTY')")
     @PostMapping("/transferMoney/")
-    public Object savingsTransferMoney(@Valid @RequestBody TransferenceDTO transferenceDTO, BindingResult bindingResult) {
+    public Object creditCardTransferMoney(@Valid @RequestBody TransferenceDTO transferenceDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(new MensajeDTO("Los campos introducidos son incorrectos"), HttpStatus.BAD_REQUEST);
         }
-        return savingsService.SavingsTransferMoney(transferenceDTO);
+        return creditCardService.creditCardTransferMoney(transferenceDTO);
     }
 
     @PreAuthorize("hasRole('ACCOUNTHOLDER')")//para borrar un Checking, solo un accountholder puede hacerlo
-    @DeleteMapping("/deleteSavings/{savingsId}")
-    public ResponseEntity<?> deleteSavings(@PathVariable("savingsId") int savingsId, @RequestBody AccountHolder accountHolder){
-        return savingsService.deleteSavings(savingsId,accountHolder);
+    @DeleteMapping("/deleteCreditCard/{creditCardId}")
+    public ResponseEntity<?> deleteCreditCard(@PathVariable("creditCardId") int creditCardId, @RequestBody AccountHolder accountHolder){
+        return creditCardService.deleteCreditCard(creditCardId,accountHolder);
     }
 
     @PreAuthorize("hasRole('ACCOUNTHOLDER')")
-    @PatchMapping("/updateSavings/{savingsId}")
-    public ResponseEntity<?> udateSavings(@PathVariable("savingsId") int savingsId, @RequestBody FactoryAccountDTO factoryAccountDTO){
-        if (!savingsService.existsByAccountHolderId(savingsId)) {
+    @PatchMapping("/updateCreditCard/{creditCardId}")
+    public ResponseEntity<?> udateCreditCard(@PathVariable("creditCardId") int creditCardId, @RequestBody FactoryAccountDTO factoryAccountDTO){
+        if (!creditCardService.existsByAccountHolderId(creditCardId)) {
             return new ResponseEntity(new MensajeDTO("La cuenta no está presente"), HttpStatus.NOT_FOUND);
         }
-        savingsService.updateSavings(savingsId, factoryAccountDTO);
-        return new ResponseEntity(new MensajeDTO("La cuenta Checking ha sido actualizada"), HttpStatus.OK);
+        creditCardService.updateCreditCard(creditCardId, factoryAccountDTO);
+        return new ResponseEntity(new MensajeDTO("La cuenta ha sido actualizada"), HttpStatus.OK);
     }
 
-
 }
+
